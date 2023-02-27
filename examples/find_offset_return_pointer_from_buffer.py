934dba982b86154ab6fc44d6d6846103c95b8bd3
Angelo942 <52466998+Angelo942@users.noreply.github.com>
2023-02-27 14:40:45 +0100

    Revert "temporarily remove examples directory"
    This reverts commit c073edf20c52661cec456e037f7df7fb01e0b874.

---
+++ examples/find_offset_return_pointer_from_buffer.py
@@ -0,0 +1,22 @@
+from pydbg import Debugger
+from pwn import *
+
+# This challenge is a simple buffer overflow
+dbg = Debugger("./challenges/start")
+
+# Start execution
+dbg.cont()
+
+dbg.p.sendline(cyclic(500))
+
+# Wait for the process to segfault
+dbg.wait()
+
+# Our input has been loaded in the instruction pointer
+pointer = dbg.ip % 2**32 #If the binary is 64bits we only take the last 4 bytes of our instruction pointer
+offset = cyclic_find(p32(pointer))
+
+print(offset)
+
+# We close this process and you can launch a new one to attack
+dbg.close()
